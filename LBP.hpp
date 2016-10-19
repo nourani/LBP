20f71b018e768793c26c2356dabf38dd628fc6cd
Navid Nourani-Vatani <Navid.Nourani-Vatani@siemens.com>
2016-10-19 09:11:06 +0200

    fixed slash direction issue

--- LBP.hpp
+++ LBP.hpp
@@ -39,7 +39,7 @@
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2\imgproc\imgproc.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 #include "LBPGPU.cuh"
 
